26e2abc73abcd97d8ba16641bf03a2825a372121
gemmaro <gemmaro.dev@gmail.com>
2025-07-19 10:37:06 +0900

    Fallback cache dir.

--- lib/pxmyportal/xdg.rb
+++ lib/pxmyportal/xdg.rb
@@ -1,5 +1,6 @@
 class PXMyPortal::XDG
-  CACHE_DIR = File.join(ENV["XDG_CACHE_HOME"], "pxmyportal")
+  CACHE_DIR = File.join(ENV["XDG_CACHE_HOME"] || File.join(Dir.home, ".cache"),
+                        "pxmyportal")
   DOC_DIR = File.join(ENV["XDG_DOCUMENTS_DIR"] || File.join(Dir.home, "Documents"),
                       "pxmyportal")
 end
